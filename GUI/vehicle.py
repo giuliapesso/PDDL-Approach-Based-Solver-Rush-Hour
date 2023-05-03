a859d865bcc37e87da0c49fa356ef601a24053a0
geanoro <g.gamo@studenti.unibs.it>
2023-05-03 16:59:21 +0200

    aggiunta addCord

--- GUI/vehicle.py
+++ GUI/vehicle.py
@@ -1,12 +1,16 @@
 class Vehicle:
     dim : int #forse non serve
     name: str
-    coords = []
+    coords = [[int,int]]
 
     def __init__(self,dim,name,coords) -> None:
         self.dim = dim
         self.name = name
         self.coords = coords
+
+    def addCoord(self,x : int , y : int):
+        self.coords.append([x,y])
+        self.dim = self.dim+1;
 
     def move(self):
         self.coords = [(0,0)]
