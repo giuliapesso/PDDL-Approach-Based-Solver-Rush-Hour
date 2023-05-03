adbeb098e7535b974cb6503703f02481f7f938a0
Giulia <giuliapesso01@gmail.com>
2023-05-03 16:42:54 +0200

    aggiunta if goal

--- GUI/ProblemParse.py
+++ GUI/ProblemParse.py
@@ -1,5 +1,7 @@
 
 import re
+
+from vehicle import Vehicle
 def drawVeichle(file):
     f = open (file,"r")
     riga = f.readlines()
@@ -10,25 +12,26 @@ def drawVeichle(file):
     yCoord = []
     findAts = []
     for r in riga : 
-        # utilizziamo
+        # permette di non leggere la parte del goal
+        if r.__contains__("goal"):
+            break
+        # si usano le espressioni regoli per ricavare le stringhe che contengono at nel problem pddl
         found = re.findall("at[\d|\w| ]+",r)
-
+        # se trovo "at" 
         if found:
-         
+            # dalla stringa rimuovo solo la parte "at" e rimane il veicolo e le coordinate x y
             found = re.sub("at","",found[0])
-            #print(found)
-            #ciao=found.split(" ")
-            #print(found.split())
-            findAts.append(found.split())
-
-        
-    
-    #print(vehicles)
-    #print(xCoord)
-    #print(yCoord)
+            findAts.append(found)
     f.close()
 
-    print(findAts)
+    #print(findAts)
+
+    for str in findAts:
+        str = str.split()
+
+        
+        #vehicles.append(Vehicle(1))
+        print(str)
 
 
 drawVeichle("v3/Problems/rushHour_Problem4x4X.pddl")
