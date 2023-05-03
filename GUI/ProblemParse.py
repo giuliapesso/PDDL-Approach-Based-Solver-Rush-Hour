2a4c794a097be9706e25668da2516ffe36d36488
geanoro <g.gamo@studenti.unibs.it>
2023-05-03 16:18:25 +0200

    parseQuasi

--- GUI/ProblemParse.py
+++ GUI/ProblemParse.py
@@ -3,24 +3,32 @@ import re
 def drawVeichle(file):
     f = open (file,"r")
     riga = f.readlines()
+    f.read()
+    print(f)
     vehicles = []
     xCoord = []
     yCoord = []
+    findAts = []
     for r in riga : 
         # utilizziamo
-        if r.__contains__("(at") :
-            """ withoutTab = r.removeprefix("        (at ")
-            withoutNewLine = withoutTab.removesuffix(")\n")
-            splitLine = withoutNewLine.split(" ") """
-            #print(splitLine)
-            splitLine = []
-            splitLine = re.split("([/d/w]+)",r)
+        found = re.findall("at[\d|\w| ]+",r)
 
-            print(splitLine)
+        if found:
+         
+            found = re.sub("at","",found[0])
+            #print(found)
+            #ciao=found.split(" ")
+            #print(found.split())
+            findAts.append(found.split())
+
+        
+    
     #print(vehicles)
     #print(xCoord)
     #print(yCoord)
     f.close()
+
+    print(findAts)
 
 
 drawVeichle("v3/Problems/rushHour_Problem4x4X.pddl")
