4c38c61e6fc2b5f8b5a64c1e042eaa193aeddfd7
geanoro <g.gamo@studenti.unibs.it>
2023-05-06 09:04:30 +0200

    ciao

--- GUI/solverScript.py
+++ GUI/solverScript.py
@@ -1,12 +1,11 @@
 import requests, sys
 
-def solve(domain, problem):
     
-        data = {'domain': open("../v3/rushHour_DomainXY.pddl", 'r').read(),
+data = {'domain': open("../v3/rushHour_DomainXY.pddl", 'r').read(),
                 'problem': open("../v3/Problems/rushHour_Problem4x4X.pddl", 'r').read()}
 
-        resp = requests.post('http://solver.planning.domains/solve',
+resp = requests.post('http://solver.planning.domains/solve',
                         verify=False, json=data).json()
 
-        with open("plan.ipc", 'w') as f:
+with open("plan.ipc", 'w') as f:
         f.write('\n'.join([act['name'] for act in resp['result']['plan']]))
